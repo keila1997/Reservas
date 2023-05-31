1995d9da3dc7a13d5e6ae0aa1ecf6f1b50b0d3f4
keila.1997 <keilaagostina@gmail.com>
2023-05-31 19:40:12 -0300

    a

--- Practica2/Program.cs
+++ Practica2/Program.cs
@@ -8,7 +8,7 @@ namespace Practica2
 {
     internal static class Program
     {
-        /// <summary>
+        /// <summary>     
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
